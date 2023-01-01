1698874025faee631c45e6afaaa681f4dea16c52
ayatkhaled <ayatbakry@yahoo.com>
2023-01-01 17:20:16 +0200

    Form Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Formm from './Component/Form';
+import Thanks from './Component/Thanks';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     
+      <Switch>
+
+        <Route exact path={"/"} component={Formm} />
+        <Route exact path={"/thanks"} component={Thanks} />
+
+
+      </Switch>
+
+    </BrowserRouter>
+
   );
 }
 
